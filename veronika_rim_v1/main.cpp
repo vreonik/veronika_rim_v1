a56b6821111776adc52151473d3858bfe4a034e0
Veronika <veronika.rimkeviciute@gmail.com>
2025-09-24 13:35:53 +0300

    Update main.cpp

--- veronika_rim_v1/main.cpp
+++ veronika_rim_v1/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -13,6 +14,7 @@ using std::left;
 using std::right;
 using std::fixed;
 using std::setprecision;
+using std::sort;
 
 struct Studentas{
     string vard;
@@ -23,11 +25,12 @@ struct Studentas{
 
 int main(){
     int n, m;
-    double sum=0;
+    double sum=0, med=0;
+    char pasirinkimas;
     Studentas stud;
     cout<<"Įveskite studento vardą: ";
     cin>>stud.vard;
-    cout<<"Įveskite sutdento pavardę: ";
+    cout<<"Įveskite studento pavardę: ";
     cin>>stud.pav;
     
     cout<<"Kiek pažymių turi studentas?: ";
@@ -38,12 +41,35 @@ int main(){
         cin>>m;
         stud.nd.push_back(m);
         sum+=m;
-        
+    }
+    
+    vector<int> nd_sortintas = stud.nd;
+    sort(nd_sortintas.begin(), nd_sortintas.end());
+    
+    if(n%2 == 0){
+        med = (nd_sortintas[n/2 - 1] + nd_sortintas[n/2]) / 2.0;
+    }
+    else{
+        med = nd_sortintas[n/2];
     }
     cout<<"Įveskite egzamino pažymį: ";
     cin>>stud.egzas;
+    
     double vidurkis = sum/n;
-    double galutinis = vidurkis*0.4 + stud.egzas*0.6;
-    cout<<fixed<<setprecision(2)<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis<<endl;
+    double galutinis_vid = vidurkis*0.4 + stud.egzas*0.6;
+    double galutinis_med = med*0.4 + stud.egzas*0.6;
+    
+    cout << "Pasirinkite galutinio balo skaičiaivimo būdą (įrašykite pirmą raidę) - vidurkis, mediana ar abudu: ";
+    cin >> pasirinkimas;
+    cout<<fixed<<setprecision(2);
+    if(pasirinkimas == 'v' || pasirinkimas == 'V'){
+        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis_vid<<endl;
+    } else if(pasirinkimas == 'm' || pasirinkimas == 'M'){
+        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Med.) "<< galutinis_med<<endl;
+    } else{
+        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis_vid<<endl;
+        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Med.) "<< galutinis_med<<endl;
+    }
+    
     
 }
