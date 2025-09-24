9cf8412ddab2db954975ca077bca8a2a1a03dbfa
Veronika <veronika.rimkeviciute@gmail.com>
2025-09-24 12:29:37 +0300

    Initial Commit

---
+++ veronika_rim_v1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  veronika_rim_v1
+//
+//  Created by Veronika  on 24/09/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
