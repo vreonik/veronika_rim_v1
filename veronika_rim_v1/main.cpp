a7d528e5278b13d98f64dc9073afff66305371cb
Veronika <veronika.rimkeviciute@gmail.com>
2025-09-24 14:32:22 +0300

    Update main.cpp

--- veronika_rim_v1/main.cpp
+++ veronika_rim_v1/main.cpp
@@ -3,18 +3,18 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
+#include <limits>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
-using std::setw;
-using std::left;
-using std::right;
 using std::fixed;
 using std::setprecision;
 using std::sort;
+using std::stringstream;
 
 struct Studentas{
     string vard;
@@ -24,52 +24,84 @@ struct Studentas{
 };
 
 int main(){
-    int n, m;
-    double sum=0, med=0;
-    char pasirinkimas;
-    Studentas stud;
-    cout<<"Įveskite studento vardą: ";
-    cin>>stud.vard;
-    cout<<"Įveskite studento pavardę: ";
-    cin>>stud.pav;
-    
-    cout<<"Kiek pažymių turi studentas?: ";
-    cin>>n;
-    
-    for(int i=0; i<n; i++){
-        cout<<"Įveskite "<<i+1<<" Pažymį iš "<<n<<": ";
-        cin>>m;
-        stud.nd.push_back(m);
-        sum+=m;
+    vector<Studentas> visi_stud;
+    char dar = 't';
+
+    while(dar == 't' || dar == 'T'){
+        Studentas stud;
+        double sum = 0, med = 0;
+
+        cout<<"Įveskite studento vardą: ";
+        cin>>stud.vard;
+        cout<<"Įveskite studento pavardę: ";
+        cin>>stud.pav;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        cout<<"Įveskite N.d. pažymius po vieną. Pabaigus įvedimą paspauskite du kartus ENTER:"<<endl;
+        while(true){
+            string eil;
+            cout<<"Pažymys: ";
+            getline(cin, eil);
+            if(eil.empty())break;
+
+            stringstream ss(eil);
+            int nd;
+            if(ss >> nd){
+                stud.nd.push_back(nd);
+                sum += nd;
+            } else{
+                cout<<"Blogai, įveskite iš naujo!:("<<endl;
+            }
+        }
+
+        cout<<"Įveskite egzamino pažymį: ";
+        cin>>stud.egzas;
+        cin.ignore();
+
+        if (stud.nd.empty()){
+            cout<<"Studentas neturi namų darbų pažymių!"<<endl;
+            continue;
+        }
+
+        double vidurkis = sum / stud.nd.size();
+
+        vector<int> nd_sortintas = stud.nd;
+        sort(nd_sortintas.begin(), nd_sortintas.end());
+        if(stud.nd.size() % 2 == 0){
+            med = (nd_sortintas[stud.nd.size() / 2 - 1] + nd_sortintas[stud.nd.size() / 2]) / 2.0;
+        } else{
+            med = nd_sortintas[stud.nd.size() / 2];
+        }
+
+        double galutinis_vid = vidurkis * 0.4 + stud.egzas * 0.6;
+        double galutinis_med = med * 0.4 + stud.egzas * 0.6;
+
+        char pasirinkimas;
+        cout<<"Pasirinkite galutinio balo skaičiavimo būdą(v-vidurkis, m-mediana, a-abudu): ";
+        cin>>pasirinkimas;
+        cin.ignore();
+
+        cout<<fixed<<setprecision(2);
+        if(pasirinkimas == 'v' || pasirinkimas == 'V'){
+            cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažymys(Vid.) "<< galutinis_vid<<endl;
+        }else if(pasirinkimas == 'm' || pasirinkimas == 'M'){
+            cout<<stud.vard<<" " <<stud.pav<<"; Galutinis pažymys(Med.) "<<galutinis_med<< endl;
+        }else{
+            cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažymys(Vid.) "<< galutinis_vid<<endl;
+            cout<<stud.vard<<" " <<stud.pav<<"; Galutinis pažymys(Med.) "<<galutinis_med<< endl;
+        }
+
+        visi_stud.push_back(stud);
+
+        cout<<"Pridėti dar vieną?(t/T - taip, kitaip - ne): ";
+        cin>>dar;
+        cin.ignore();
     }
-    
-    vector<int> nd_sortintas = stud.nd;
-    sort(nd_sortintas.begin(), nd_sortintas.end());
-    
-    if(n%2 == 0){
-        med = (nd_sortintas[n/2 - 1] + nd_sortintas[n/2]) / 2.0;
+
+    cout<<"Visų studentų sąrašas:"<<endl;
+    for (auto temp : visi_stud) {
+        cout<<temp.vard<<" "<<temp.pav<<"; N.D. skaičius: "<<temp.nd.size()<<endl;
     }
-    else{
-        med = nd_sortintas[n/2];
-    }
-    cout<<"Įveskite egzamino pažymį: ";
-    cin>>stud.egzas;
-    
-    double vidurkis = sum/n;
-    double galutinis_vid = vidurkis*0.4 + stud.egzas*0.6;
-    double galutinis_med = med*0.4 + stud.egzas*0.6;
-    
-    cout << "Pasirinkite galutinio balo skaičiaivimo būdą (įrašykite pirmą raidę) - vidurkis, mediana ar abudu: ";
-    cin >> pasirinkimas;
-    cout<<fixed<<setprecision(2);
-    if(pasirinkimas == 'v' || pasirinkimas == 'V'){
-        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis_vid<<endl;
-    } else if(pasirinkimas == 'm' || pasirinkimas == 'M'){
-        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Med.) "<< galutinis_med<<endl;
-    } else{
-        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis_vid<<endl;
-        cout<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Med.) "<< galutinis_med<<endl;
-    }
-    
-    
+
+    return 0;
 }
