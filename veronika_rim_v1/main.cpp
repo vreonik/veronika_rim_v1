a47865f968ebd448fc492e710aa7ae8e43ce7c1e
Veronika <veronika.rimkeviciute@gmail.com>
2025-09-24 13:08:14 +0300

    Update main.cpp

--- veronika_rim_v1/main.cpp
+++ veronika_rim_v1/main.cpp
@@ -1,14 +1,49 @@
-//
-//  main.cpp
-//  veronika_rim_v1
-//
-//  Created by Veronika  on 24/09/2025.
-//
-
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+using std::fixed;
+using std::setprecision;
+
+struct Studentas{
+    string vard;
+    string pav;
+    vector<int> nd;
+    int egzas;
+};
+
+int main(){
+    int n, m;
+    double sum=0;
+    Studentas stud;
+    cout<<"Įveskite studento vardą: ";
+    cin>>stud.vard;
+    cout<<"Įveskite sutdento pavardę: ";
+    cin>>stud.pav;
+    
+    cout<<"Kiek pažymių turi studentas?: ";
+    cin>>n;
+    
+    for(int i=0; i<n; i++){
+        cout<<"Įveskite "<<i+1<<" Pažymį iš "<<n<<": ";
+        cin>>m;
+        stud.nd.push_back(m);
+        sum+=m;
+        
+    }
+    cout<<"Įveskite egzamino pažymį: ";
+    cin>>stud.egzas;
+    double vidurkis = sum/n;
+    double galutinis = vidurkis*0.4 + stud.egzas*0.6;
+    cout<<fixed<<setprecision(2)<<stud.vard<<" "<<stud.pav<<"; Galutinis pažimys(Vid.) "<< galutinis<<endl;
+    
 }
